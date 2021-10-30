a4231cb021ef35d449c75f53258be5a3911406a4
KimKiYoung <rlarldud6482@naver.com>
2021-10-30 23:32:20 +0900

    메인뷰 만들기

--- SwfitUIText/SwfitUIText/MainView.swift
+++ SwfitUIText/SwfitUIText/MainView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section(header: Text("섹션1")){
+                ForEach(1...5, id:\.self){index in
+                    CellView(icon: "book.fill", title: "책을 읽으세요  \(index)", start: "pm 1", end: "pm 10", color: Color.pink)
+                }.listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
+            }.listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
+            
+            Section(header: Text("섹션2")){
+                ForEach(1...5, id:\.self){index in
+                    CellView(icon: "heart.fill", title: "하트 읽으세요  \(index)", start: "am 3", end: "am 7", color: Color.yellow)
+                }
+            }.listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
+        }.listStyle(.plain)
     }
 }
 
