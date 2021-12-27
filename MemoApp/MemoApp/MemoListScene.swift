ff592867ddf09dcf344e2c27bfbecf894490aa1e
KimKiYoung <rlarldud6482@naver.com>
2021-12-27 09:49:22 +0900

    DateFormatter List Date에 처리

--- MemoApp/MemoApp/MemoListScene.swift
+++ MemoApp/MemoApp/MemoListScene.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 struct MemoListScene: View {
     @EnvironmentObject var store: MemoStore
-    
+    @EnvironmentObject var formatter: DateFormatter
     
     var body: some View {
         NavigationView {
             List(store.list) { memo in
-                Text(memo.content)
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1)
+                    
+                    Text("\(memo.insertDate, formatter: self.formatter)")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                        
+                }
             }
             .navigationBarTitle("너의 메모")
         }
@@ -25,5 +34,6 @@ struct MemoListScene_Previews: PreviewProvider {
     static var previews: some View {
         MemoListScene()
             .environmentObject(MemoStore())
+            .environmentObject(DateFormatter.memoDateFormatter)
     }
 }
