852e79b2c8d252af65e410b695d65516e7f810a1
KimKiYoung <rlarldud6482@naver.com>
2021-12-27 14:30:44 +0900

    MemoListScene 유아이 구현

--- MemoApp/MemoApp/Scene/MemoListScene.swift
+++ MemoApp/MemoApp/Scene/MemoListScene.swift
@@ -11,13 +11,32 @@ struct MemoListScene: View {
     @EnvironmentObject var store: MemoStore
     @EnvironmentObject var formatter: DateFormatter
     
+    @State private var showComposer: Bool = false
+    
     var body: some View {
         NavigationView {
             List(store.list) { memo in
                 MemoCell(memo: memo)
             }
             .navigationBarTitle("너의 메모")
+            .navigationBarItems(trailing: ModalButton(show: $showComposer))
+            .sheet(isPresented: $showComposer, content: {
+                ComposeScene(show: self.$showComposer
+                )
+            })
         }
+    }
+}
+
+fileprivate struct ModalButton: View {
+    @Binding var show: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.show = true
+        }, label: {
+            Image(systemName: "plus")
+        })
     }
 }
 
