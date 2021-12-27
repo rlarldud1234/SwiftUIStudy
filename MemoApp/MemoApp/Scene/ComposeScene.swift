fddc45382e4a54874b16ca0066162d58ca3f4c4a
KimKiYoung <rlarldud6482@naver.com>
2021-12-27 14:30:23 +0900

    ComposeScene 저장기능 구현

--- MemoApp/MemoApp/Scene/ComposeScene.swift
+++ MemoApp/MemoApp/Scene/ComposeScene.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct ComposeScene: View {
+    @EnvironmentObject var store: MemoStore
+    @State private var content: String = ""
+    
+    @Binding var show: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                TextField("", text: $content)
+                    .background(Color.gray)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .navigationBarTitle("메모 추가하기", displayMode: .inline)
+            .navigationBarItems(leading: DismissButton(show: $show), trailing: SetButton(show: $show, content: $content))
+        }
+    }
+}
+
+fileprivate struct DismissButton: View {
+    @Binding var show: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.show = false
+        }, label: {
+            Text("취소")
+        })
+    }
+}
+
+fileprivate struct SetButton: View {
+    @Binding var show: Bool
+    
+    @EnvironmentObject var store: MemoStore
+    @Binding var content: String
+    
+    var body: some View {
+        Button(action: {
+            self.store.insert(memo: self.content)
+            
+            self.show = false
+        }, label: {
+            Text("저장")
+        })
     }
 }
 
 struct ComposeScene_Previews: PreviewProvider {
     static var previews: some View {
-        ComposeScene()
+        ComposeScene(show: .constant(false))
+            .environmentObject(MemoStore())
     }
 }
