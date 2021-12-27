edb2f7ed7cabee48311ef391853cc3ff8937489a
KimKiYoung <rlarldud6482@naver.com>
2021-12-27 14:30:14 +0900

    메모 추가하는 기능 composeScene에 구현

---
+++ MemoApp/MemoApp/Scene/ComposeScene.swift
@@ -0,0 +1,20 @@
+//
+//  ComposeScene.swift
+//  MemoApp
+//
+//  Created by 김기영 on 2021/12/27.
+//
+
+import SwiftUI
+
+struct ComposeScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ComposeScene_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposeScene()
+    }
+}
