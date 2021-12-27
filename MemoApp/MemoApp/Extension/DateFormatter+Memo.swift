5e72ff7f4e126a2bf2126e557ac54dfca7edbd93
KimKiYoung <rlarldud6482@naver.com>
2021-12-27 09:48:57 +0900

    Extension DateFormatter 처리

--- MemoApp/MemoApp/Extension/DateFormatter+Memo.swift
+++ MemoApp/MemoApp/Extension/DateFormatter+Memo.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension DateFormatter {
+    static let memoDateFormatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .none
+        f.locale = Locale(identifier: "Ko_kr")
+        return f
+    }()
+}
+
+extension DateFormatter: ObservableObject {
+    
+}
